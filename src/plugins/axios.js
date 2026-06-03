21375a0b58d5c240854c988baefa0f44d412728d
JohannaMiranda <mirandajohanna88@gmail.com>
2026-06-03 08:21:09 -0300

    modyfy: plugins/axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  withCredentials: true,
+  baseURL: 'http://127.0.0.1:8000/api',
+  withCredentials: false,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
