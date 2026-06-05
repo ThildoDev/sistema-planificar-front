b53c390b529fa48c7785b2f8e9f0e49a34d53284
JohannaMiranda <mirandajohanna88@gmail.com>
2026-06-05 20:47:31 -0300

    import los styles globales y de componentes para la aplicación

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/styles/global.css'
+import '@/assets/styles/themes.css'
+import '@/assets/styles/components/buttons.css'
+import '@/assets/styles/components/inputs.css'
+import '@/assets/styles/components/cards.css'
+
+
 const app = createApp(App)
 
 app.use(createPinia())
